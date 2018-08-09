3d45545bdafd2cf6cbd2c99d6166600886d53c73
christinaalenda <clenda041@gmail.com>
2018-08-09 19:20:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 # dog.rb
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    this_dogs_name = "Lassie"
   end
   
   def name
@@ -9,4 +9,3 @@ class Dog
   end
 end
 
-Lassie = this_dogs_name
